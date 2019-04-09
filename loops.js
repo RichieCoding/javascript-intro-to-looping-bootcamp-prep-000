eeeccd282226f411e3de734780936cd961bbbb7e
Richard Li <richardli125@gmail.com>
2019-04-09 18:27:22 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,6 +16,15 @@ function whileLoop(n) {
   return 'done'
 }
 
+var i = 0
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
 function doWhileLoop(num) {
-  
+  do {
+    console.log('')
+  }
 }
